cffb8cf30acbbf7c4360fc03d0c42cccdddd120b
eloicasamayor <37459666+eloicasamayor@users.noreply.github.com>
2021-08-08 18:32:02 +0200

    working on animation

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
 
 const kBlackColor = Color(0xff121212);
+const kLightGreyColor = Color(0xaaaaaaaa);
+const kDarkGreyColor = Color(0xdd555555);
 const kPinkColor = Color(0xffef5550);
--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+
 import 'package:ui_challange_travel_info_app/constants.dart';
 
 class MyHomePage extends StatefulWidget {
@@ -65,6 +66,9 @@ class _MyHomePageState extends State<MyHomePage>
               _TopText(),
               _DesertContainer(),
               _SquareButton(),
+              _VerticalDots(),
+              _ArrowLines(),
+              _IconRow(),
             ],
           ),
         ),
@@ -131,8 +135,8 @@ class _DesertContainer extends StatelessWidget {
     return Consumer<Animation<double>>(
       builder: (context, animation, child) {
         return Positioned(
-          top: 150,
-          left: 25 + 30.0 * animation.value,
+          top: 150 - 10 * animation.value,
+          left: 35 + 30.0 * animation.value,
           right: 40 * (1 - animation.value),
           child: Container(
             width: 90,
@@ -167,38 +171,191 @@ class _SquareButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Positioned(
-      top: 210,
-      right: 20,
-      child: Stack(
-        children: [
-          Transform.translate(
-            offset: const Offset(-10, -10),
-            child: Container(
-              height: 80,
-              width: 80,
-              decoration: BoxDecoration(
-                color: Colors.black.withOpacity(.4),
+    return Consumer<Animation<double>>(
+      builder: (context, animation, child) {
+        return Positioned(
+          top: 180 - 70 * animation.value,
+          right: 20,
+          child: Stack(
+            children: [
+              Transform.translate(
+                offset: Offset(
+                    -10 * (1 - animation.value), -10 * (1 - animation.value)),
+                child: Container(
+                  height: 80,
+                  width: 80,
+                  decoration: BoxDecoration(
+                    color: Colors.black.withOpacity(.4),
+                  ),
+                ),
               ),
+              Container(
+                height: 80,
+                width: 80,
+                decoration: const BoxDecoration(
+                  color: Colors.black,
+                ),
+                child: IconButton(
+                  onPressed: () => {},
+                  icon: const Icon(
+                    Icons.add,
+                    color: Colors.white,
+                    size: 35,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
+
+class _VerticalDots extends StatelessWidget {
+  const _VerticalDots({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<Animation<double>>(
+      builder: (context, animation, child) {
+        return Positioned(
+          top: 280,
+          left: 20,
+          child: FadeTransition(
+            opacity: animation,
+            child: Column(
+              children: [
+                _CircleDot(selected: true),
+                SizedBox(
+                  height: 5 + 15 * animation.value,
+                ),
+                _CircleDot(),
+                SizedBox(
+                  height: 5 + 15 * animation.value,
+                ),
+                _CircleDot(),
+              ],
             ),
           ),
-          Container(
-            height: 80,
-            width: 80,
-            decoration: const BoxDecoration(
-              color: Colors.black,
-            ),
-            child: IconButton(
-              onPressed: () => {},
-              icon: const Icon(
-                Icons.add,
-                color: Colors.white,
-                size: 35,
+        );
+      },
+    );
+  }
+}
+
+class _CircleDot extends StatelessWidget {
+  final bool selected;
+  const _CircleDot({
+    Key? key,
+    this.selected = false,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(5),
+      height: 20,
+      width: 20,
+      decoration: selected
+          ? BoxDecoration(
+              border: Border.all(
+                color: kLightGreyColor,
               ),
-            ),
-          ),
-        ],
+              shape: BoxShape.circle,
+              //color: Colors.grey,
+            )
+          : null,
+      child: const CircleAvatar(
+        backgroundColor: kLightGreyColor,
       ),
+    );
+  }
+}
+
+class _ArrowLines extends StatelessWidget {
+  const _ArrowLines({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<Animation<double>>(
+      builder: (context, animation, child) {
+        return Positioned(
+          top: 525,
+          left: 66,
+          child: FadeTransition(
+            opacity: animation,
+            child: Container(
+              width: 150,
+              height: 2,
+              child: Row(
+                children: [
+                  SizedBox(
+                    width: 60 - 60 * animation.value,
+                  ),
+                  Expanded(
+                    child: Container(
+                      color: kDarkGreyColor,
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 30,
+                  ),
+                  Expanded(
+                    child: Container(
+                      color: kDarkGreyColor,
+                    ),
+                  ),
+                  SizedBox(
+                    width: 60 - 60 * animation.value,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
+
+class _IconRow extends StatelessWidget {
+  const _IconRow({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<Animation<double>>(
+      builder: (context, animation, child) {
+        return Positioned(
+          top: 600,
+          left: 70,
+          child: Row(
+            children: [
+              const Icon(
+                Icons.map,
+                size: 30,
+                color: kDarkGreyColor,
+              ),
+              SizedBox(
+                width: 20,
+              ),
+              const Icon(
+                Icons.sailing,
+                size: 30,
+                color: kDarkGreyColor,
+              ),
+              SizedBox(
+                width: 20,
+              ),
+              const Icon(
+                Icons.baby_changing_station,
+                size: 30,
+                color: kDarkGreyColor,
+              ),
+            ],
+          ),
+        );
+      },
     );
   }
 }
